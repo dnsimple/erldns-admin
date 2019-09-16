7dbb71b12c7b2121a6103951f38a5892849a1e9a
Anthony Eden <me@anthonyeden.com>
2019-09-16 14:25:38 -0400

    cowboy:start_clear function takes 3 arguments and uses map syntax

--- src/erldns_admin.erl
+++ src/erldns_admin.erl
@@ -70,7 +70,7 @@ init([]) ->
     ]
   ),
 
-  {ok, _} = cowboy:start_clear(?MODULE, 10, [{port, port()}], [{env, [{dispatch, Dispatch}]}]),
+  {ok, _} = cowboy:start_clear(?MODULE, [{port, port()}], #{env => #{dispatch => Dispatch}}),
 
   {ok, #state{}}.
 
